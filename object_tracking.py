f260a272770948de8920154c1418e8c4829e3f05
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-10-26 15:05:33 -0300

    Update object_tracking.py

--- object_tracking.py
+++ object_tracking.py
@@ -2,58 +2,57 @@ import cv2
 import time
 import math
 
+# Abra o vídeo e verifique se a abertura foi bem-sucedida
 video = cv2.VideoCapture("bb3.mp4")
+if not video.isOpened():
+    print("Erro ao abrir o vídeo")
+    exit()
 
-# Load tracker 
+# Inicialize o tracker (rastreador)
 tracker = cv2.TrackerCSRT_create()
 
-# Read the first frame of the video
+# Leia o primeiro quadro do vídeo
 returned, img = video.read()
 
-# Select the bounding box on the image
-bbox = cv2.selectROI("Tracking", img, False)
+# Selecione a região de interesse (ROI) na imagem
+bbox = cv2.selectROI("Rastreamento", img, False)
 
-# Initialise the tracker on the img and the bounding box
+# Inicialize o tracker na imagem e na região de interesse
 tracker.init(img, bbox)
 
-print(bbox)
+print("BBox (região de interesse):", bbox)
 
+# Função para desenhar a caixa delimitadora (bounding box) e texto
 def drawBox(img, bbox):
     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
 
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 3, 1)
+    cv2.putText(img, "Rastreando", (75, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-
+# Função para realizar o rastreamento do objetivo
 def goal_track(img, bbox):
-    x, y,w, h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
 
-    #################
-    # ADD CODE HERE #
-    #################
 
+# Loop principal
 while True:
-    
-    check, img = video.read()   
+    check, img = video.read()
 
-    # Update the tracker on the img and the bounding box
+    # Atualize o tracker na imagem e na região de interesse
     success, bbox = tracker.update(img)
 
     if success:
         drawBox(img, bbox)
     else:
-        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        cv2.putText(img, "Perdido", (75, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-    #################
-    # ADD CODE HERE #
-    #################
+    cv2.imshow("Resultado", img)
 
-    cv2.imshow("result", img)
-            
     key = cv2.waitKey(25)
-    if key == 32:
-        print("Stopped")
+    if key == 32:  # Tecla de espaço para parar
+        print("Parado")
         break
 
+# Libere o vídeo e feche todas as janelas
 video.release()
-cv2.destroyALLwindows()
+cv2.destroyAllWindows()  # "destroyALLwindows" corrigido para "destroyAllWindows"
